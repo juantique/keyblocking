dbdf100b20f471f6a1ddf0f04957f05835edbc16
juanconde <jconde48@uan.edu.co>
2019-09-27 00:58:08 -0500

    Correcciones de modelos, del archivo models.py

--- keyblocking/Apps/keymanager/admin.py
+++ keyblocking/Apps/keymanager/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from keyblocking.Apps.keymanager.models import *
-# Register your models here.
-admin.site.register(login)
-admin.site.register(usuario)
-admin.site.register(registro)
-admin.site.register(listado)
-admin.site.register(formulario_pagina)
-admin.site.register(Recuperacion)
+# REGISTRO DE LOS MODELOS.
+admin.site.register(Tblusuario)
+admin.site.register(Tbltipodocumento)
+admin.site.register(Tblciudad)
+admin.site.register(Tblgenero)
+admin.site.register(Tblpagina)
--- keyblocking/Apps/keymanager/models.py
+++ keyblocking/Apps/keymanager/models.py
@@ -2,58 +2,41 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
-class login (models.Model):
-    id_usuario = models.BigIntegerField()
-    correo_activo = models.EmailField()
-    contraseña = models.CharField(max_length=100)
 
-class usuario(models.Model):
-    login = models.BigIntegerField()
+class Tblusuario(models.Model):
+    # Id_Usuario(PK)
+    # Id_Pagina(FK)
+    # Id_Tipo_Documento(FK)
+    # Id_Genero(FK)
+    # Id_Ciudad(FK)
+    Nombre_Usuario = models.CharField(max_length=35)
     Apellido_Parterno = models.CharField(max_length=35)
     Apellido_Materno = models.CharField(max_length=35)
-    Nombre=models.CharField(max_length=35)
-    correo_activo = models.EmailField()
-    FechaNacimiento = models.DateTimeField(auto_now_add=True)
-    SEXO = (('F', 'Femenino'), ('M', 'Masculino'))
-    Sexo = models.CharField(max_length=1, choices=SEXO, default='M')
-    Ciudad = models.CharField(max_length=35)
-
-class registro(models.Model):
-    id_usuario = models.BigIntegerField()
-    Apellido_Parterno = models.CharField(max_length=35)
-    Apellido_Materno = models.CharField(max_length=35)
-    Nombre=models.CharField(max_length=35)
-    Telefono = models.IntegerField()
+    No_Documento = models.CharField(max_length=35)
+    Contraseña = models.CharField(max_length=100)
+    Correo_Usuario = models.EmailField()
+    Fecha_Nacimiento = models.DateTimeField(auto_now_add=True)
+    Telefono = models.CharField(max_length=35)
     Direccion_Domicilio = models.CharField(max_length=35)
-    correo_activo = models.EmailField()
-    correo_alternativo = models.EmailField()
-    contraseña = models.CharField(max_length=100)
-    FechaNacimiento = models.DateTimeField(auto_now_add=True)
-    SEXO = (('F', 'Femenino'), ('M', 'Masculino'))
-    Sexo = models.CharField(max_length=1, choices=SEXO, default='M')
-    Ciudad = models.CharField(max_length=35)
+    Correo_Alterno = models.EmailField()
 
-class listado(models.Model):
-    id_usuario = models.BigIntegerField()
-    Apellido_Parterno = models.CharField(max_length=35)
-    Apellido_Materno = models.CharField(max_length=35)
-    Nombre=models.CharField(max_length=35)
-    id_pagina = models.BigIntegerField()
+class Tbltipodocumento(models.Model):
+    #Id_Tipo_Documento(PK)
+    Descripcion_Documento = models.CharField(max_length=35)
+    
+class Tblciudad(models.Model):
+    #Id_Ciudad(PK)
+    Descripcion_Ciudad = models.CharField(max_length=35)
 
-class formulario_pagina(models.Model):
-    id_pagina = models.BigIntegerField()
-    #PAGINARED = (('F','Facebook'), ('I','Instagram'), ('T','Twitter'), ('L','Linkedln'), ('YT','YouTube'), ('S','Snapchat'), ('P','Pinterest'), ('NA''Ninguna'))
-    PAGINARED = (('F','Facebook'), ('I','Instagram'))
-    Pagina_Red = models.CharField(max_length=1, choices=PAGINARED, default = 'N/A')
-    #PAGINACOMP = (('A','Amazon'), ('E','Ebay'), ('W','Walmart'), ('A','Alibaba'), ('ML','MercadoLibre'), ('AE','AliExpress'), ('BB','Best Bay'), ('NA''Ninguna'))
-    PAGINACOMP = (('A','Amazon'), ('E','Ebay'))
-    Pagina_Compra = models.CharField(max_length=35, choices=PAGINACOMP, default = 'N/A')
-    Correo_Cuenta = models.EmailField()
-    contraseña = models.CharField(max_length=100)
-    Descripcion_Cuenta = models.TextField(max_length=400)
+class Tblgenero(models.Model):
+    #Id_Genero(PK)
+    Descripcion_Genero = models.TextField(max_length=400)
 
-class Recuperacion(models.Model):
-    correo_activo = models.EmailField()
-    correo_alternativo = models.EmailField()
-    id_usuario = models.BigIntegerField()
-    Telefono = models.IntegerField()
+class Tblpagina(models.Model):
+    #Id_Pagina(PK)
+    #Id_Usuario(FK)
+    Nombre_Pagina = models.CharField(max_length=35)
+    Url_Pagina = models.CharField(max_length=35)
+    Correo_Pagina = models.EmailField()
+    Contraseña_Pagina = models.CharField(max_length=100)
+    Descripcion_Pagina = models.CharField(max_length=100)
